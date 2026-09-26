08a59cdb52562b4d9ddb657415cd93f5a90117e3
WaldrichLee12 <lee.waldrich@gmail.com>
2026-09-26 14:22:40 +1000

    Completed daily simulation metrics

--- backend/src/services/simulationService.js
+++ backend/src/services/simulationService.js
@@ -13,10 +13,30 @@ function simulateDay(day) {
 
     let totalTransactions = 0;
     let unitsSold = 0;
+    let students = 0;
+    let officeWorkers = 0;
+    let residents = 0;
+    let largestBasket = 0;
 
     for (let i = 0; i < customerCount; i++) {
         const customerType = generateCustomerType(isWeekend);
         const basketSize = generateBasketSize(customerType);
+
+        if (basketSize > largestBasket){
+            largestBasket = basketSize;
+        }
+
+        if (customerType === "Student"){
+            students++;
+        }
+
+        if (customerType === "Office Workers"){
+            officeWorkers++;
+        }
+
+        if (customerType === "Resident"){
+            residents++;
+        }
 
     revenue += basketSize * 5;
     cogs += basketSize * 5;
@@ -29,9 +49,14 @@ function simulateDay(day) {
 
     const grossProfit = revenue - cogs;
     const averageTransactionValue = revenue / totalTransactions;
+    const averageBasketSize = unitsSold / totalTransactions;
+
+    const dayType = isWeekend ? "Weekend" : "Weekday";
+
 
     return {
         day,
+        dayType,
         customerCount,
         totalTransactions,
         unitsSold,
@@ -39,6 +64,11 @@ function simulateDay(day) {
         cogs,
         grossProfit,
         averageTransactionValue,
+        averageBasketSize,
+        students,
+        officeWorkers,
+        residents,
+        largestBasket,
     };
 }
 
